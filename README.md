4543e7cbfe301a2d0cf338af2185a336e17b6d56
Trend20 <enockomondi305@gmail.com>
2023-12-13 09:55:17 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learning Go
+
+Initialize a Go module by running: `go mod init <module name> `
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// variable
+	var conferenceName = "Go Conference"
+	// constant variable
+	const venueName = "Nairobi Theater"
+	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Println("All are welcome to", venueName)
+}
