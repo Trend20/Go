615f18a0395258594f9dda766a65003f2822f5a2
Trend20 <enockomondi305@gmail.com>
2023-12-22 13:39:16 +0300

    values

--- hello/hello-world.go
+++ hello/hello-world.go
@@ -1,3 +1,4 @@
+// This is the most basic Golang program
 package main
 
 import "fmt"
--- values/values.go
+++ values/values.go
@@ -1,3 +1,6 @@
+// Go has various value types including STRINGS, INTEGERS, FLOATS, BOOLEANS etc.
+// strings can be concatenated using the "+" operator e.g: "Hello" + "World" = Hello World
+
 package main
 
 import "fmt"
@@ -7,4 +10,8 @@ func main() {
 	fmt.Println("1 + 1 =", 1+1)
 	fmt.Println("go" + "lang")
 	fmt.Println("7.0 / 3.0 =", 7.0/3.0)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
 }
