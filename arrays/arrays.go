cc578ffc47fa531e8b3da50eb3c66fc6c66cbf55
Trend20 <enockomondi305@gmail.com>
2024-01-08 19:27:15 +0300

    arrays in Go

---
+++ arrays/arrays.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	ages := [4]int{1, 2, 3, 5}
+	fmt.Println(ages)
+
+	names := [5]string{"mike", "billy", "montase", "sonko", "log"}
+	fmt.Println(names)
+
+}
