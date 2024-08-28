383a098a7745a1394bf9cc74c2511d8e9bdbe499
Trend20 <enockomondi305@gmail.com>
2024-08-28 09:33:33 +0300

    functions in go

--- tour_of_go/functions/functions.go
+++ tour_of_go/functions/functions.go
@@ -6,6 +6,8 @@ func Functions() {
 	fmt.Println("This is how Functions work in Go!")
 	studentsName := [6]string{"Mike", "Brian", "Bon", "Collins", "Brenda"}
 	printNames(studentsName)
+	fmt.Println(addInt(3, 4))
+	fmt.Println(showNames("mary", "kevin"))
 }
 
 // function to print names
@@ -14,4 +16,22 @@ func printNames(names [6]string) string {
 		fmt.Println(i, name)
 	}
 	return ""
+}
+
+// a function that adds two integer numbers
+// When two or more consecutive named function parameters share a type, you can omit the type from all but the last.
+func addInt(x, y int) int {
+	return x + y
+}
+
+// function with multiple returns
+func showNames(x, y string) (string, string) {
+	return x, y
+}
+
+// named returns
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
 }
