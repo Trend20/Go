2928516c438b37937788352dbea3d25af6aa69d5
Trend20 <enockomondi305@gmail.com>
2024-08-28 12:02:34 +0300

    switch statements

--- tour_of_go/control_structures/switch.go
+++ tour_of_go/control_structures/switch.go
@@ -1,6 +1,9 @@
 package control_structures
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func GoSwitch() {
 	name := []string{"John", "Paul", "George", "Ringo"}
@@ -14,4 +17,36 @@ func GoSwitch() {
 	default:
 		fmt.Println("Hey all")
 	}
+}
+
+func SwitchDay() {
+	fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("In two days.")
+	default:
+		fmt.Println("Too far away.")
+	}
+}
+
+// defer statement
+func Defer() {
+	defer fmt.Println("world")
+	fmt.Println("hello")
+}
+
+// Stacking defers
+// Deferred function calls are pushed onto a stack. When a function returns, its deferred calls are executed in last-in-first-out order.
+// To learn more about defer statements read this blog post.
+func StackedDefer() {
+	fmt.Println("counting")
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+	fmt.Println("done")
 }
